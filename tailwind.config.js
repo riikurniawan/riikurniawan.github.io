f084254c67b566b175d546a0e6b8c05822fdb7e7
Ari Kurniawan <arikurnia2601@gmail.com>
2025-05-17 11:40:34 +0700

    Update

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,13 +1,8 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-    content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"], // Sesuaikan path
-    theme: {
-      extend: {
-        fontFamily: {
-          rubik: ["Rubik", "sans-serif"],
-        },
-      },
-    },
-    plugins: [],
-  };
-  
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"], // Sesuaikan path
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
